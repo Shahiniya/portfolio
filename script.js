c4c4099678d25e65ff902eb6930f382e0bba9149
Shelli <sh.panjieva@gmail.com>
2022-07-03 10:26:54 +0900

    new

--- script.js
+++ script.js
@@ -21,13 +21,26 @@ navigationItem.forEach(navItem=>{
     })
 })
 
-const scrollBtn = document.querySelector('.scrollToTop-btn')
-windoow.addEventListener('scroll', ()=>{
-    scrollBtn.classList.toggle('active',window.scrollY > 500)
-})
-    scrollBtn.addEventListener('click',()=>{
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    })
+// const scrollBtn = document.querySelector('.scrollToTop-btn')
+// windoow.addEventListener('scroll', ()=>{
+//     scrollBtn.classList.toggle('active',window.scrollY > 500)
+// })
+//     scrollBtn.addEventListener('click',()=>{
+//         document.body.scrollTop = 0;
+//         document.documentElement.scrollTop = 0;
+//     })
+
+// window.addEventListener('scroll',()=>{
+//     let reveals = documnet.querySelectorAll('.reveal')
+//     for(let i=0; i<reveals.length; i++){
+//         let windowHeight = window.innerHeight;
+//         let revealTop = reveals[i].getBoundingClientRect().top;
+//         let revealPoint = 50;
+//         if(revealTop < windowHeight - revealPoint){
+//             reveals[i].classList.add('.active')
+//         }
+//     }
+// })
+
 
 })
